2f24c29e6ea1187c4382154791d4e596698cc280
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-05-25 10:05:25 -0500

    Start with variables

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Assignment code here
-
-
+var numberBase = "1234567"
+var passwordBase = ""
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(){
+  console.log("Generate password funciton")
+  var passwordLength = prompt("How long do you want your password to be")
+  console.log(passwordLength)
+
+  var userWantsNumbers = confirm("Do you want numbers in your password")
+  console.log(userWantsNumbers)
+  // if user say yes to wanting numbers
+  if (userWantsNumbers = true){
+    passwordBase = passwordBase.concat(numberBase)
+  }
+  // return passwordLength
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
