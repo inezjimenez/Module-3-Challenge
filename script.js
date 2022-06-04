6f20afc6ac435602f67d074c8e531d6037bfaec7
Inez  Mercedes Jimenez <ijim9182@yahoo.com>
2022-06-04 16:31:17 -0500

    updated  functions

--- script.js
+++ script.js
@@ -3,85 +3,117 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var passwordCriteria={
-//Created arrays of all possible character choices
-passwordLength=0;
-numbers = ['0','1','2','3','4','5','6','7','8','9'];
-special= ['!','@','#','$','%','&','*','+','=','?','~'];
-lowerCase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
-upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
-}
 
-console.log(length);
+//Created arrays of all possible character choices
+var passwordLength=0;
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var special= ['!','@','#','$','%','&','*','+','=','?','~'];
+var lowerCase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperCase= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var allCharacters= [] 
+var finalPassword=""
+
+console.log(passwordLength);
 console.log(numbers);
 console.log(special);
 console.log(lowerCase);
 console.log(upperCase);
 
-function writePassword(){
 
-  var password=generatePassword();
 
-  var passwordText=document.querySelector("#password");
+//   while(passwordLength<8||passwordLength>=8){
+//     passwordLength=prompt("How many characters do you want your password to be? Passworod must be between 1 and 8 characters.")
 
-  passwordText.value-password;
+//     if( passwordLength>8){
+//       alert("Password must be between 1 and 8 characters.")
+//   }
+//   else {
+//     showPrompts();
+
+//     while (passwordCriteria.passwordLength<passwordLength){
+
+//       if (lowerCase===false && upperCase===false && numbers===false && special===false){
+//         alert("You must select at least one criteria of lowercase,uppercase,numbers or special characters.")
+//         showPrompts();
+//       }
+//       else{
+
+//       }
+//     }
+//   }
+
+//   return responses;
+// }
+
+function usersChoice(){
+  // var passwordOptions=questions();
+  // var possibleCombo=[];
+  // var finalPassword="";
+  var passwordLength= parseInt(
+    prompt("How many characters will your password have?")
+    )
+    console.log(passwordLength)
+
+  var numbers= confirm(
+    "Do you want numbers in your password?"
+  )
+
+  var passwordChoices = {
+    passLength: passwordLength,
+    numChoice: numbers,
+    usersSpecial: special,
+    usersLowerCase: lowerCase,
+    usersUperCase: upperCase,
+
+  }
+  console.log(passwordChoices)
+
+
+return passwordChoices;
 }
 
 function generatePassword(){
-  var result=""
-  var passwordLength=0;
-  var numbers;
-  var special;
-  var loerCase;
-  var upperCase;
+  var usersPassword= usersChoice()
 
-  while(passwordLength<8||passwordLength>=8){
-    passwordLength=prompt("How many characters do you want your password to be? Passworod must be between 1 and 8 characters.")
-
-    if( passwordLength>8){
-      alert("Password must be between 1 and 8 characters.")
+  if (usersPassword.numChoice){
+    allCharacters= allCharacters.concat(numbers)
+    console.log(allCharacters)
   }
 
-  return responses;
+  if (usersPassword.usersSpecial){
+    allCharacters=allCharacters.concat(special)
+    console.log(allCharacters)
+  }
+
+  if(usersPassword.usersLowerCase){
+    allCharacters=allCharacters.concat(lowerCase)
+    console.log(lowerCase)
+  }
+
+  if(usersPassword.usersUperCase){
+    allCharacters=allCharacters.concat(upperCase)
+    console.log(upperCase)
+  }
+
+  if(usersPassword.)
+
+
+
+
+
+
+
+
+  for (var i=0;i<usersPassword.passLength;i++){
+    finalPassword+= allCharacters[Math.floor(Math.random() * allCharacters.length)];
+    console.log(finalPassword)
+  }
+
+
+
+return finalPassword
 }
 
-function generatePassword(){
-  var passwordOptions=questions();
-  var possibleCombo=[];
-  var finalPassword="";
-
-
-
-  // if user say yes to wanting numbers
-  if (passwordOptions.askNumbers= true){
-    possibleCombo = possibleCombo.concat(askNumbers)
-  }
-
-  //if user says yes to wanting special characters
-  if(passwordOptions.askSpecial=true){
-    possibleCombo=possibleCombo.concat(askSpecial)
-  }
-
-  //if user says yes to wanting lowercase letters
-  if(passwordOptions.askLowerCase=true){
-    possibleCombo=possibleCombo.concat(askLowerCase)
-  }
-
-  //if user says yes to wanting uppercase letters
-  if(passwordOptions.askUpperCase=true){
-    possibleCombo=possibleCombo.concat(askUpperCase)
-  }
-
-  console.log(possibleCombo);
-
-  for(var i=0;i<passwordOptions.length;i++){
-    finalPassword+=possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
-  }
-  console.log(finalPassword);
-
-  // return passwordLength
-  return finalPassword;
-}
 
 
 // Write password to the #password input
